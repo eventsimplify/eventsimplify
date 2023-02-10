d806452d569784edaeee54594a9b8cc7fdbcabb7
nishanprime <thapanishan9@gmail.com>
2023-02-10 18:17:58 -0500

    navbar struture

---
+++ frontend/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div className="">
+      <input
+        className="bg-white p-2 rounded-lg border-2 border-gray-400"
+        type="text"
+        placeholder="Search"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
