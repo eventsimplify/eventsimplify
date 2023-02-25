5bcc5b64d67ddc948aa1e63eb9573b556369f192
nishanprime <thapanishan9@gmail.com>
2023-02-25 01:46:57 -0500

    added DO postgres db

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,5 +1,6 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import fs from 'fs';
 
 // entity imports
 import {
@@ -10,28 +11,64 @@ import {
   Ticket,
   Invitations,
   Role,
-} from "../entity";
+} from '../entity';
+const caCert = fs.readFileSync(
+  '/Users/nishanthapa/Documents/EventSimplifyRemasteredBackend/backend/src/config/ca-certificate.crt'
+);
 //subscribers imports
-import { EventEntitySubscriber } from "../subscribers";
+import { EventEntitySubscriber } from '../subscribers';
+const dbHostLocation = process.env.CHOOSE_DB_HOST;
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "bipulpoudel",
-  password: "paramount10",
-  database: "eventsimplify",
-  synchronize: true,
-  logging: false,
-  entities: [
-    User,
-    Event,
-    Organization,
-    OrganizationUser,
-    Ticket,
-    Invitations,
-    Role,
-  ],
-  migrations: [],
-  subscribers: [EventEntitySubscriber],
-});
+// if dbHostLocation == "digitalocean", set AppDataSource to this else null
+
+let AppDataSource;
+if (dbHostLocation === 'digitalocean') {
+  AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'eventsimplify-postgres-db-do-user-12466944-0.b.db.ondigitalocean.com',
+    port: 25060,
+    username: 'doadmin',
+    password: 'AVNS_DNVkKA7XXedS-Hgqufg',
+    database: 'defaultdb',
+    ssl: {
+      rejectUnauthorized: true,
+      ca: caCert,
+    },
+    synchronize: true,
+    logging: false,
+    entities: [
+      User,
+      Event,
+      Organization,
+      OrganizationUser,
+      Ticket,
+      Invitations,
+      Role,
+    ],
+    migrations: [],
+    subscribers: [EventEntitySubscriber],
+  });
+} else {
+  AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'bipulpoudel',
+    password: 'paramount10',
+    database: 'eventsimplify',
+    synchronize: true,
+    logging: false,
+    entities: [
+      User,
+      Event,
+      Organization,
+      OrganizationUser,
+      Ticket,
+      Invitations,
+      Role,
+    ],
+    migrations: [],
+    subscribers: [EventEntitySubscriber],
+  });
+}
+export { AppDataSource };
