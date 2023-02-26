141d2cf4c21629aef2e711c0048eb6e0cce73cb7
nishanprime <thapanishan9@gmail.com>
2023-02-25 22:38:00 -0500

    used persistent volume to preserve state between cross build and make db faster

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,5 +1,5 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 
 // entity imports
 import {
@@ -10,37 +10,65 @@ import {
   Ticket,
   Invitations,
   Role,
-} from "../entity";
+} from '../entity';
 //subscribers imports
-import { EventEntitySubscriber } from "../subscribers";
+import { EventEntitySubscriber } from '../subscribers';
 
 //importing ssl certificate
-import fs from "fs";
-import path from "path";
-const caCert = fs.readFileSync(path.join(__dirname, "certificate.crt"));
+import fs from 'fs';
+import path from 'path';
+let AppDataSource;
+if (process.env.CHOOSE_DB_HOST === 'docker_container') {
+  AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'postgres', // This should be the name of the Docker service in your `docker-compose.yml` file
+    port: 5432, // This is the default Postgres port
+    username: 'postgres', // This is the default Postgres username
+    password: 'mysecretpassword', // This is the password you set for the `postgres` user in your `docker-compose.yml` file
+    database: 'mydb', // This is the name of the database you want to use
+    synchronize: true,
+    logging: false,
+    entities: [
+      User,
+      Event,
+      Organization,
+      OrganizationUser,
+      Ticket,
+      Invitations,
+      Role,
+    ],
+    migrations: [],
+    subscribers: [EventEntitySubscriber],
+  });
+}
+// else if (process.env.CHOOSE_DB_HOST === 'digitalocean') {
+//   const caCert = fs.readFileSync(path.join(__dirname, 'certificate.crt'));
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: caCert,
-  },
-  synchronize: true,
-  logging: false,
-  entities: [
-    User,
-    Event,
-    Organization,
-    OrganizationUser,
-    Ticket,
-    Invitations,
-    Role,
-  ],
-  migrations: [],
-  subscribers: [EventEntitySubscriber],
-});
+//   AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     ssl: {
+//       rejectUnauthorized: true,
+//       ca: caCert,
+//     },
+//     synchronize: true,
+//     logging: false,
+//     entities: [
+//       User,
+//       Event,
+//       Organization,
+//       OrganizationUser,
+//       Ticket,
+//       Invitations,
+//       Role,
+//     ],
+//     migrations: [],
+//     subscribers: [EventEntitySubscriber],
+//   });
+// }
+
+export { AppDataSource };
