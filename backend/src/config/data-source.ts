1062a08bc03b02421d6a2d94f2eb4c12c68a7ba1
nishanprime <thapanishan9@gmail.com>
2023-03-06 19:22:22 -0500

    so the issue was with in house db and constant changes that conflicts with persistent volume since persistent volume won't let new initilization to take place but code requires new changes thus conflict happens so rather create db in diff server

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -11,17 +11,14 @@ import fs from 'fs';
 import path from 'path';
 const caCert = fs.readFileSync(path.join(__dirname, 'certificate.crt'));
 
+const PGHOST = 'mydb.ep-blue-haze-947702.ap-southeast-1.aws.neon.tech';
+const PGDATABASE = 'neondb';
+const PGUSER = 'eventsimplify';
+const PGPASSWORD = 'GurmJK1NR2jc';
+const ENDPOINT_ID = 'mydb';
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'postgres',
-  port: 5432, // This is the default Postgres port
-  username: 'postgres', // This is the default Postgres username
-  password: 'mysecretpassword', // This is the password you set for the `postgres` user in your `docker-compose.yml` file
-  database: 'mydb',
-  ssl: process.env.NODE_ENV === 'production' && {
-    rejectUnauthorized: true,
-    ca: caCert,
-  },
+  url: 'postgres://eventsimplify:GurmJK1NR2jc@ep-blue-haze-947702.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
   synchronize: true,
   logging: false,
   entities: [...Object.values(Entities)],
